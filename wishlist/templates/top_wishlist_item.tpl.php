05546ad9c10a44b5e753f686d60f66d386f9a9f2
Bela Barabas <FSCYLEX\bela.barabas@ro-barabasb.cylex.local>
2016-08-24 15:05:47 +0300

    add product list an

---
+++ wishlist/templates/top_wishlist_item.tpl.php
@@ -0,0 +1,16 @@
+
+    <div class="row">
+        <div class="col-md-4">
+            <span>
+                <?php  echo $name;?>
+            </span>
+        </div>
+        <div class="col-md-4">
+            <span>
+                <?php  echo $price;?>
+            </span>
+        </div>
+        <div class="col-md-4">
+            <?php  echo $image;?>
+        </div>
+    </div>
