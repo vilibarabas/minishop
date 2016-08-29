f295716a53477fe6b4a3e4ddc61ffdeda0c7c37e
Bela Barabas <FSCYLEX\bela.barabas@ro-barabasb.cylex.local>
2016-08-29 18:25:46 +0300

    update

---
+++ wishlist/templates/wishlist_top_container.tpl.php
@@ -0,0 +1,4 @@
+<div class="dropdown2">
+      <button class="dropbtn2">Wishlist <span class="badge"><?php echo $count; ?></span> <span class="glyphicon glyphicon-star"></span></button>
+      <div class="dropdown-content2"><?php echo $top; ?></div>
+</div>
